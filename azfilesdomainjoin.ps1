b92aa7b18eed3e5c0a44b75d64c594da04d6d17d
kellystephen-10 <66949078+kellystephen-10@users.noreply.github.com>
2020-12-02 14:23:01 +0000

    added modules and relative path for script

--- azfilesdomainjoin.ps1
+++ azfilesdomainjoin.ps1
@@ -1,12 +1,28 @@
+
+
+#Enter parameters for the script
+param(
+    [Parameter(Mandatory = $True,valueFromPipeline=$true)][String] $SubscriptionId,
+    [Parameter(Mandatory = $True,valueFromPipeline=$true)][String] $StorageAccountName,
+    [Parameter(Mandatory = $True,valueFromPipeline=$true)][String] $ResourceGroupName
+    )
+
+#Install required modules
+
+Install-Module -Name PowerShellGet -Force
+Install-module -Name AZ -force
+
+#Set execution policy to run script
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -force
 
-$dir = 'c:\temp\'
-
-
-Invoke-WebRequest -Uri 
+# Download the file containing the module from Github
+$dir = 'c:\windows\temp\'
+Invoke-WebRequest -Uri "https://github.com/asptechpublic/azfileshybrid/blob/main/files/azfileshybrid.zip?raw=true" -OutFile $dir\azfileshybrid.zip 
+Expand-Archive -path $dir\azfileshybrid.zip -force
 
 # Navigate to where AzFilesHybrid is unzipped and stored and run to copy the files into your path
-.\CopyToPSPath.ps1 
+Set-Location 'c:\windows\temp\azfileshybrid\'
+Invoke-expression "c:\windows\temp\azfileshybrid\CopyToPSPath.ps1" 
 
 #Import AzFilesHybrid module
 Import-Module -Name AzFilesHybrid
@@ -14,20 +30,8 @@ Import-Module -Name AzFilesHybrid
 #Login with an Azure AD credential that has either storage account owner or contributer Azure role assignment
 Connect-AzAccount
 
-#Define parameters
-$SubscriptionId = "46406297-35ea-4e3a-afea-7d0e8e15307e"
-$ResourceGroupName = "pearts-remote"
-$StorageAccountName = "csestor"
-
 #Select the target subscription for the current session
 Select-AzSubscription -SubscriptionId $SubscriptionId 
 
-# Register the target storage account with your active directory environment under the target OU (for example: specify the OU with Name as "UserAccounts" or DistinguishedName as "OU=UserAccounts,DC=CONTOSO,DC=COM"). 
-# You can use to this PowerShell cmdlet: Get-ADOrganizationalUnit to find the Name and DistinguishedName of your target OU. If you are using the OU Name, specify it with -OrganizationalUnitName as shown below. If you are using the OU DistinguishedName, you can set it with -OrganizationalUnitDistinguishedName. You can choose to provide one of the two names to specify the target OU.
-# You can choose to create the identity that represents the storage account as either a Service Logon Account or Computer Account (default parameter value), depends on the AD permission you have and preference. 
-# Run Get-Help Join-AzStorageAccountForAuth for more details on this cmdlet.
-
+#Join the storageaccount to the domain
 Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroupName  -StorageAccountName $StorageAccountName -DomainAccountType "ComputerAccount"
-
-#-OrganizationalUnitDistinguishedName "<ou-distinguishedname-here>" <# If you don't provide the OU name as an input parameter, the AD identity that represents the storage account is created under the root directory. #> `
-#-EncryptionType "<AES256/RC4/AES256,RC4>" <# Specify the encryption agorithm used for Kerberos authentication. Default is configured as "'RC4','AES256'" which supports both 'RC4' and 'AES256' encryption. #>
