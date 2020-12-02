386af387cb4996d4b6b01b47e2673a79f87f64a1
kellystephen-10 <66949078+kellystephen-10@users.noreply.github.com>
2020-12-02 15:35:00 +0000

    working script

--- azfilesdomainjoin.ps1
+++ azfilesdomainjoin.ps1
@@ -1,5 +1,4 @@
 
-
 #Enter parameters for the script
 param(
     [Parameter(Mandatory = $True,valueFromPipeline=$true)][String] $SubscriptionId,
@@ -7,22 +6,26 @@ param(
     [Parameter(Mandatory = $True,valueFromPipeline=$true)][String] $ResourceGroupName
     )
 
+#Create a new directory to download
+New-Item -Path 'c:\temp\' -itemtype directory -force
+
 #Install required modules
 
 Install-Module -Name PowerShellGet -Force
 Install-module -Name AZ -force
 
 #Set execution policy to run script
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Currentuser
 
 # Download the file containing the module from Github
-$dir = 'c:\windows\temp\'
-Invoke-WebRequest -Uri "https://github.com/asptechpublic/azfileshybrid/blob/main/files/azfileshybrid.zip?raw=true" -OutFile $dir\azfileshybrid.zip 
-Expand-Archive -path $dir\azfileshybrid.zip -force
+Invoke-WebRequest -Uri "https://github.com/asptechpublic/azfileshybrid/blob/main/files/azfileshybrid.zip?raw=true" -OutFile 'c:\temp\azfileshybrid.zip'
+
+Expand-Archive -literalpath 'c:\temp\azfileshybrid.zip' -destinationpath 'c:\temp\azfileshybrid\' -Force
 
 # Navigate to where AzFilesHybrid is unzipped and stored and run to copy the files into your path
-Set-Location 'c:\windows\temp\azfileshybrid\'
-Invoke-expression "c:\windows\temp\azfileshybrid\CopyToPSPath.ps1" 
+Set-Location -path 'c:\temp\azfileshybrid\'
+
+Invoke-expression "c:\temp\azfileshybrid\CopyToPSPath.ps1" 
 
 #Import AzFilesHybrid module
 Import-Module -Name AzFilesHybrid
@@ -33,5 +36,5 @@ Connect-AzAccount
 #Select the target subscription for the current session
 Select-AzSubscription -SubscriptionId $SubscriptionId 
 
-#Join the storageaccount to the domain
+#Join storage account to the domain
 Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroupName  -StorageAccountName $StorageAccountName -DomainAccountType "ComputerAccount"
