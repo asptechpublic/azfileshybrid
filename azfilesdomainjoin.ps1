3561b28ceac052905196c56636173d752a3d5acf
kellystephen-10 <66949078+kellystephen-10@users.noreply.github.com>
2020-12-02 11:22:33 +0000

    create script and upload zip file

---
+++ azfilesdomainjoin.ps1
@@ -0,0 +1,33 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -force
+
+$dir = 'c:\temp\'
+
+
+Invoke-WebRequest -Uri 
+
+# Navigate to where AzFilesHybrid is unzipped and stored and run to copy the files into your path
+.\CopyToPSPath.ps1 
+
+#Import AzFilesHybrid module
+Import-Module -Name AzFilesHybrid
+
+#Login with an Azure AD credential that has either storage account owner or contributer Azure role assignment
+Connect-AzAccount
+
+#Define parameters
+$SubscriptionId = "46406297-35ea-4e3a-afea-7d0e8e15307e"
+$ResourceGroupName = "pearts-remote"
+$StorageAccountName = "csestor"
+
+#Select the target subscription for the current session
+Select-AzSubscription -SubscriptionId $SubscriptionId 
+
+# Register the target storage account with your active directory environment under the target OU (for example: specify the OU with Name as "UserAccounts" or DistinguishedName as "OU=UserAccounts,DC=CONTOSO,DC=COM"). 
+# You can use to this PowerShell cmdlet: Get-ADOrganizationalUnit to find the Name and DistinguishedName of your target OU. If you are using the OU Name, specify it with -OrganizationalUnitName as shown below. If you are using the OU DistinguishedName, you can set it with -OrganizationalUnitDistinguishedName. You can choose to provide one of the two names to specify the target OU.
+# You can choose to create the identity that represents the storage account as either a Service Logon Account or Computer Account (default parameter value), depends on the AD permission you have and preference. 
+# Run Get-Help Join-AzStorageAccountForAuth for more details on this cmdlet.
+
+Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroupName  -StorageAccountName $StorageAccountName -DomainAccountType "ComputerAccount"
+
+#-OrganizationalUnitDistinguishedName "<ou-distinguishedname-here>" <# If you don't provide the OU name as an input parameter, the AD identity that represents the storage account is created under the root directory. #> `
+#-EncryptionType "<AES256/RC4/AES256,RC4>" <# Specify the encryption agorithm used for Kerberos authentication. Default is configured as "'RC4','AES256'" which supports both 'RC4' and 'AES256' encryption. #>
